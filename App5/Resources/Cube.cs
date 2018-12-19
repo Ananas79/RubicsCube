62e4779ce1aaa5fb8eeedef6c7bbf9fbc515d112
root <bayandink@yandex.ru>
2018-12-19 23:03:50 +0500

    + Лабораторная 1. Создание окон Photo и CubeModel

--- App5/Resources/Cube.cs
+++ App5/Resources/Cube.cs
@@ -16,16 +16,69 @@ namespace App5
     [Activity(Label = "Cube")]
     public class Cube : Activity
     {
+        CubeAnimationView cb;
+        LinearLayout mLayout;
         const int ORANGE = 0, GREEN = 1, YELLOW = 2, BLUE = 3, RED = 4, WHITE = 5, BROWN = 6;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Title = "Сборка";
-  
+            SetContentView(Resource.Layout.CubeActivityLayout);
+            mLayout = FindViewById<LinearLayout>(Resource.Id.cubeLayout);
+            cb = new CubeAnimationView(this);
+
+            string type = Intent.GetStringExtra("type");
+            switch (type)
+            {
+                case "photo":
+                    cb.SetState(Intent.GetIntArrayExtra("state"));
+                    cb.SolveCurrentState();
+                    break;
+                case "server":
+                    var cs = new CubeSolver();
+                    
+                    string f = CubeSolver.Backward(CubeSolver.Unzip(Intent.GetStringExtra("server")));
+                    cs.DoFormula(f);
+                    cb.SetState(cs.GetState());
+                    cb.SolveCurrentState();
+                    break;
+                case "algo":
+                    
+                    cb.SetState(Intent.GetIntArrayExtra("state"));
+                    cb.DoFormulas(new string[1] { Intent.GetStringExtra("formula") });
+                    break;
+            }
+
             //cb.SetState(Intent.GetIntArrayExtra("state"));
 
+            cb.LayoutParameters = new ViewGroup.LayoutParams
+            (
+                    ViewGroup.LayoutParams.MatchParent,
+                    ViewGroup.LayoutParams.MatchParent
+            );
+            mLayout.AddView(cb);
 
            
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            
+            this.MenuInflater.Inflate(Resource.Menu.menu1, menu);
+            if (menu != null && Intent.GetStringExtra("type") == "algo")
+                menu.SetGroupVisible(Resource.Id.main_menu_group, false);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.share:
+                    return true;
+            }
+            return base.OnContextItemSelected(item);
         }
 
         public override void OnBackPressed()
